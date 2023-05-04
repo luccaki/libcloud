34b132879627ef4815a2ab08ef38542969f64e20
luccaki <lucca.ianaguivara@gmail.com>
2023-05-04 00:19:41 -0300

    added MediaIoBaseUpload

--- libcloud/storage/drivers/google_drive.py
+++ libcloud/storage/drivers/google_drive.py
@@ -37,7 +37,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload, MediaIoBaseUpload
 from google.oauth2.service_account import Credentials as ServiceAccountCredentials
 
 
